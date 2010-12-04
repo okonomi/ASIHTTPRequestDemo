1893978bf5da7aaeaa697b5219f80a3dd765617b
okonomi <oknm@okonomi.sakura.ne.jp>
2010-12-04 15:01:39 +0900

    Set NavigationController title.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -15,14 +15,11 @@
 #pragma mark -
 #pragma mark View lifecycle
 
-/*
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.title = @"ASIHTTPRequest Demo";
 }
-*/
 
 /*
 - (void)viewWillAppear:(BOOL)animated {
